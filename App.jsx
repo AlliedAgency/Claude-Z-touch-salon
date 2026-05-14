f03d67deee24222913a010d2c6a6e1cc605c4d23
AlliedAgency <alliedagencyy@gmail.com>
2026-05-14 17:17:01 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,31 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Services from './components/Services'
+import Gallery from './components/Gallery'
+import Team from './components/Team'
+import Pricing from './components/Pricing'
+import Reviews from './components/Reviews'
+import Instagram from './components/Instagram'
+import FAQ from './components/FAQ'
+import Contact from './components/Contact'
+import BookingCTA from './components/BookingCTA'
+import Footer from './components/Footer'
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Services />
+      <Gallery />
+      <Team />
+      <Pricing />
+      <Reviews />
+      <Instagram />
+      <FAQ />
+      <Contact />
+      <BookingCTA />
+      <Footer />
+    </>
+  )
+}
